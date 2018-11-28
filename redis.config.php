663f8e61a420992b6cc6c64e5ca1760093ca4a84
Nathan Thisler <macgeek312@icloud.com>
2018-11-28 08:30:20 -0500

    Update redis.config.php

--- redis.config.php
+++ redis.config.php
@@ -1,6 +1,10 @@
-<php $CONFIG = array ('memcache.locking' => '\\OC\\Memcache\\Redis',
- 'redis' => array(
+<php $CONFIG = array (
+     'memcache.locking' => '\\OC\\Memcache\\Redis',
+     'memcache.local' => '\\OC\\Memcache\\Redis',
+     'memcache.distributed' => '\\OC\\Memcache\\Redis',
+     'redis' => array(
          'host' => 'redis',
          'port' => 6379,
- ),
-);
+      ),
+  );
+?>
